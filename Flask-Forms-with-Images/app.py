8dcba881c531152f42d48500e8058e9f48a7d6f3
galinasu23-meet <109904723+galinasu23-meet@users.noreply.github.com>
2022-11-30 19:11:15 +0200

    Update app.py
    no

--- Flask-Forms-with-Images/app.py
+++ Flask-Forms-with-Images/app.py
@@ -3,11 +3,25 @@ from flask import session as login_session
 import pyrebase
 import os
 
-app = Flask(  # Create a flask app
-    __name__,
+Config = {
+  apiKey: "AIzaSyCNjzcYiTKZe2nyQxhw_UfbEhbPk7Ki17I",
+  authDomain: "meet-21653.firebaseapp.com",
+  databaseURL: "https://meet-21653-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "meet-21653",
+  storageBucket: "meet-21653.appspot.com",
+  messagingSenderId: "1082594692785",
+  appId: "1:1082594692785:web:d897a71e121216567a5c90",
+  measurementId: "G-DYQ56JFKE9"
+  databaseURL : "https://meet-21653-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+app = Flask(__name__)
     template_folder='templates',  # Name of html file folder
     static_folder='static'  # Name of directory for static files
-)
 
 UPLOAD_FOLDER = 'static/images/posts'
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
